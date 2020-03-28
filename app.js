8bb3c86f7a20a978b96e598b51f5433ea9556642
Laura De la Rosa <laura.dlr.cardona@gmail.com>
2020-03-28 15:34:12 -0500

    commit para subir json

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const PORT = process.env.PORT || 3000;
 const bookDatabase = require('./connection');
 const app = express();
 
-app.use(morgan(process.env.NODE_ENV !== 'production' ? 'dev' : 'combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
