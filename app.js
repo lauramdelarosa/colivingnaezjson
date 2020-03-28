1536005b25e928d0e889c11936c647f82fafa69d
Laura De la Rosa <laura.dlr.cardona@gmail.com>
2020-03-28 14:58:51 -0500

    commiting project with changes

--- app.js
+++ app.js
@@ -10,32 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/api/v1/books', (req, res) => {
-  bookDatabase('book')
-    .select('*')
-    .then(books => res.status(200).json(books))
-    .catch(error => {
-      res.status(500).json({ error: error.message, stack: error.stack });
-    });
-});
-
-app.get('/api/v1/books/:id', (req, res) => {
-  bookDatabase('book')
-    .select('*')
-    .limit(1)
-    .where({ id: req.params.id })
-    .then(book => res.status(200).json(book))
-    .catch(error => {
-      res.status(500).json({ error: error.message, stack: error.stack });
-    });
-});
-
-app.post('/api/v1/books', (req, res) => {
-  bookDatabase('book')
-    .insert(req.body)
-    .then(() => res.status(201).json({ data: req.body }))
-    .catch(error => {
-      res.status(500).json({ error: error.message, stack: error.stack });
-    });
+  response.send('Warming up friend.');
 });
 
 app.listen(PORT, () => console.log('Example app is listening!'));
